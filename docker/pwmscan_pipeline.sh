dd950ee3e63da1a70ad8f74ac55946165476cd4b
smoretti <sebastien.moretti@sib.swiss>
2022-09-09 18:05:13 +0200

    Add bowtie docker command

--- docker/pwmscan_pipeline.sh
+++ docker/pwmscan_pipeline.sh
@@ -33,7 +33,8 @@ GENOME="${g}"
 MYPID=$BASHPID
 
 #NOTE Comment to use local (not in container) commands
-DOCKER_CMD="docker run --rm --mount type=bind,source=`realpath $WORK_DIR`,target=/work_dir --mount type=bind,source=`realpath $BOWTIE_DIR`,target=/bowtie_dir sibswiss/pwmscan:1.1.9"
+#NOTE -i is required for docker to read piped stdin
+DOCKER_CMD="docker run --rm -i --mount type=bind,source=`realpath $WORK_DIR`,target=/work_dir --mount type=bind,source=`realpath $BOWTIE_DIR`,target=/bowtie_dir sibswiss/pwmscan:1.1.9"
 
 
 #TODO what to log in $WORK_DIR/pwmscan_${MYPID}.log???
@@ -53,8 +54,7 @@ $DOCKER_CMD matrix_prob            -b 0.29,0.21,0.21,0.29 /work_dir/$INPUT_MATRI
 
 # Command pipeline
 #FIXME where this *unmapped.dat* file comes from?
-$DOCKER_CMD mba -c 1475 /work_dir/$INPUT_MATRIX | $DOCKER_CMD awk '{print ">"$2"\n"$1}' > $WORK_DIR/awk_${MYPID}.txt
-#$DOCKER_CMD mba -c 1475 /work_dir/$INPUT_MATRIX | $DOCKER_CMD awk '{print ">"$2"\n"$1}' | $DOCKER_CMD bowtie --threads 4 -l11 -n0 -a /bowtie_dir/$GENOME -f - --un /work_dir/unmapped.dat
+$DOCKER_CMD mba -c 1475 /work_dir/$INPUT_MATRIX | $DOCKER_CMD awk '{print ">"$2"\n"$1}' | $DOCKER_CMD bowtie --threads 4 -l11 -n0 -a /bowtie_dir/$GENOME -f - --un /work_dir/unmapped.dat
 #mba -c 1475 /home/local/www-ccg/weekly/pwmfile_hg38_37886_39604 | awk '{print ">"$2"\n"$1}' | bowtie --threads 4 -l11 -n0 -a $bowtieDir/h_sapiens_hg38 -f - --un wwwtmp/unmapped.dat | sort --parallel=5 -T LOCAL_TMP -s -k3,3V -k4,4n -k2,2 | bowtie2bed -s hg38 -l 11 | filterOverlaps -l11 | awk 'BEGIN { while((getline line < "$workDir/matrixScore_tab_37886_39604.txt") > 0 ) {split(line,f," "); pvalue[f[1]]=f[2]} close("$workDir/matrixScore_tab_37886_39604.txt")} {print $1"  "$2"    "$3"    "$4"    "$5"    "$6"    ""MA0137.3 STAT1""  ""P-value="pvalue[$5]}' > $workDir/$pwmscanFile.bed
 
 exit 0
